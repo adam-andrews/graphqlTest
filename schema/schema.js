3dbd424016048603d42601a3165f8ceaed66671e
Adam Andrews <andrewsadam07@gmail.com>
2022-05-15 13:20:33 +0100

    Added Hobby Type and query

--- schema/schema.js
+++ schema/schema.js
@@ -1,21 +1,46 @@
 const graphql = require('graphql');
 var _ = require('lodash');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+	GraphQLObjectType,
+	GraphQLID,
+	GraphQLString,
+	GraphQLInt,
+	GraphQLSchema,
+} = graphql;
 // Create Types
 
 var users = [
-	{ id: "1", name: 'John Doe', age: 30 },
-	{ id: "2", name: 'Jane Doe', age: 25 },
-	{ id: "3", name: 'Jack Doe', age: 20 },
+	{ id: '1', name: 'John Doe', age: 30, profession: 'Web Developer' },
+	{ id: '2', name: 'Jane Doe', age: 25, profession: 'Web Designer' },
+	{ id: '3', name: 'Jack Doe', age: 20, profession: 'Web Designer' },
 ];
 
-const userType = new graphql.GraphQLObjectType({
+var hobbies = [
+    { id: '1', title: 'Coding', description: '1' },
+    { id: '2', title: 'Reading', description: '1' },
+    { id: '3', title: 'Sleeping', description: '2' },
+    { id: '4', title: 'Eating', description: '2' },
+
+]
+
+const userType = new GraphQLObjectType({
 	name: 'User',
 	description: 'This represents a user',
 	fields: () => ({
-		id: { type: GraphQLString },
+		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		age: { type: GraphQLInt },
+		profession: { type: GraphQLString },
+	}),
+});
+
+const HobbyType = new GraphQLObjectType({
+	name: 'Hobby',
+	description: 'This represents a hobby',
+	fields: () => ({
+		id: { type: GraphQLID },
+		title: { type: GraphQLString },
+		description: { type: GraphQLString },
 	}),
 });
 
@@ -25,13 +50,23 @@ const RootQuery = new GraphQLObjectType({
 	fields: {
 		user: {
 			type: userType,
-			args: { id: { type: GraphQLString } },
+			args: { id: { type: GraphQLID } },
 			resolve(parents, args) {
 				//we resolve with daya
 				// get and return data with source
 				return _.find(users, { id: args.id });
 			},
 		},
+        hobby:{
+            type:HobbyType,
+            args: { id: { type: GraphQLID } },
+            resolve(parents, args) {
+				//we resolve with daya
+				// get and return data with source
+				return _.find(hobbies, { id: args.id });
+			},
+
+        }
 	},
 });
 

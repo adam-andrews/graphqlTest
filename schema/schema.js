66794a9f0ab95b7add357ec994095faf3ff8c72e
Adam Andrews <andrewsadam07@gmail.com>
2022-05-15 12:56:56 +0100

    Added resolve

--- schema/schema.js
+++ schema/schema.js
@@ -1,39 +1,40 @@
 const graphql = require('graphql');
-
-const {
-	GraphQLObjectType,
-	GraphQLString,
-	GraphQLInt,
-    GraphQLSchema
-} = graphql;
+var _ = require('lodash');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 // Create Types
+
+var users = [
+	{ id: "1", name: 'John Doe', age: 30 },
+	{ id: "2", name: 'Jane Doe', age: 25 },
+	{ id: "3", name: 'Jack Doe', age: 20 },
+];
 
 const userType = new graphql.GraphQLObjectType({
 	name: 'User',
 	description: 'This represents a user',
 	fields: () => ({
-		id: {type: GraphQLString},
-		name: {type: GraphQLString},
-		age: {type: GraphQLInt},
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt },
 	}),
 });
 
-
 const RootQuery = new GraphQLObjectType({
-    name:"RootQueryType",
-    description:'Root Query',
-    fields:{
-        user:{
-            type:userType,
-            args:{id:{type:GraphQLString}},
-            resolve(parents,args){
-                //we resolve with daya
-                // get and return data with source
-            }
-        }
-    }, 
-})
+	name: 'RootQueryType',
+	description: 'Root Query',
+	fields: {
+		user: {
+			type: userType,
+			args: { id: { type: GraphQLString } },
+			resolve(parents, args) {
+				//we resolve with daya
+				// get and return data with source
+				return _.find(users, { id: args.id });
+			},
+		},
+	},
+});
 
-modeule.exports = new GraphQLSchema({
-    query:RootQuery,
-}) 
+module.exports = new GraphQLSchema({
+	query: RootQuery,
+});
